b21565a9f8320979de2ee77b6e4d16263f15dc1b
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-05-20 15:54:02 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -276,33 +276,47 @@ export default function Home() {
                                 <h2>Devis</h2>
                             </div>
                             <div className="container wrapper-booking flex-row">
-                                <div className="booking-form">
-                                    <form id="bookingForm" method="POST">
+                                <div className="bookong-form">
+                                    <form method="POST">
                                         <div className="wrapper-input flex-row">
-                                            <input type="text" id="name" name="name" placeholder="Prénom" />
-                                            <input type="text" id="lastname" name="lastname" placeholder="Nom" />
+                                            <input type="text" name="name" placeholder="Prénom" />
+                                            <input
+                                                type="text"
+                                                name="lastname"
+                                                placeholder="Nom"
+                                            />
                                         </div>
+                                        {/*/.wrapper-input*/}
                                         <div className="wrapper-input flex-row">
-                                            <input type="text" id="phone" name="phone" placeholder="Numéro de téléphone" />
-                                            <input type="text" id="email" name="email" placeholder="Adresse email" />
+                                            <input type="text" name="phone" placeholder="Numéro de téléphone" />
+                                            <input type="text" name="email" placeholder="Adresse email" />
                                         </div>
+                                        {/*/.wrapper-input*/}
                                         <div className="wrapper-input flex-row">
-                                            <input type="text" id="city" name="city" placeholder="Ville d'exercice" />
-                                            <input type="text" id="specialty" name="specialty" placeholder="Votre spécialité paramédicale" />
+                                            <input type="text" name="date" placeholder="Ville d'exercice" />
+                                            <input type="text" name="time" placeholder="Votre spécialité paramédicale" />
                                         </div>
+                                        {/*/.wrapper-input*/}
                                         <div className="wrapper-input flex-row">
-                                            <input type="text" id="message" name="message" placeholder="Message" className="large-input" />
+                                            <input type="text" name="date" placeholder="Message" className="large-input" />
                                         </div>
-                                        <button className="btn" id="sendbook" type="submit">Obtenez un devis gratuitement</button>
+                                        <button className="btn" id="sendbook" type="submit">
+                                            Obtenez  un devis gratuitement
+                                        </button>
                                         <p className="res-booking" />
                                     </form>
                                 </div>
+                                {/*/.bookong-form*/}
                                 <div className="booking-working-hours">
                                     <h3>Nous sommes heureux de vous proposer un devis gratuit pour nos services.</h3>
-                                    <p>Afin de mieux répondre à vos besoins, veuillez remplir le formulaire ci-dessous. Plus vous nous fournirez d'informations, plus nous serons en mesure de vous proposer un devis précis et adapté à vos besoins</p>
-                                </div>
-                            </div>
+                                    <p>
+                                        Afin de mieux répondre à vos besoins, veuillez remplir le formulaire ci-dessous. Plus vous nous fournirez d'informations, plus nous serons en mesure de vous proposer un devis précis et adapté à vos besoins
 
+                                    </p>
+
+                                </div>
+                                {/*/.booking-working-hours*/}
+                            </div>
                             {/*/.wrapper-booking*/}
                         </section>
                         {/* ====== End Booking ====== */}
