28cbe162c339e12d7924b509c82efc76d5d325f1
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-05-27 00:13:44 +0100

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import "./styles/responsive.css";
 import AboutUs from "@/components/AboutUs";
 import Service from "@/components/Service";
 import Offers from "@/components/Offers";
+import Devis from "@/components/Devis";
 
 export default function Home() {
     useDynamicStyle("/styles/responsive.css");
@@ -120,55 +121,7 @@ export default function Home() {
                             </section>
                             {/* ====== End FAQ ====== */}
                             {/* ====== Booking ====== */}
-                            <section id="booking" className="ptb">
-                                <div className="container">
-                                    {/* <span className="suptitle">Booking</span> */}
-                                    <h2>Devis</h2>
-                                </div>
-                                <div className="container wrapper-booking flex-row">
-                                    <div className="bookong-form">
-                                        <form method="POST">
-                                            <div className="wrapper-input flex-row">
-                                                <input type="text" name="name" placeholder="Prénom" />
-                                                <input
-                                                    type="text"
-                                                    name="lastname"
-                                                    placeholder="Nom"
-                                                />
-                                            </div>
-                                            {/*/.wrapper-input*/}
-                                            <div className="wrapper-input flex-row">
-                                                <input type="text" name="phone" placeholder="Numéro de téléphone" />
-                                                <input type="text" name="email" placeholder="Adresse email" />
-                                            </div>
-                                            {/*/.wrapper-input*/}
-                                            <div className="wrapper-input flex-row">
-                                                <input type="text" name="date" placeholder="Ville d'exercice" />
-                                                <input type="text" name="time" placeholder="Votre spécialité paramédicale" />
-                                            </div>
-                                            {/*/.wrapper-input*/}
-                                            <div className="wrapper-input flex-row">
-                                                <input type="text" name="date" placeholder="Message" className="large-input" />
-                                            </div>
-                                            <button className="btn" id="sendbook" type="submit">
-                                                Obtenez  un devis gratuitement
-                                            </button>
-                                            <p className="res-booking" />
-                                        </form>
-                                    </div>
-                                    {/*/.bookong-form*/}
-                                    <div className="booking-working-hours">
-                                        <h3>Nous sommes heureux de vous proposer un devis gratuit pour nos services.</h3>
-                                        <p>
-                                            Afin de mieux répondre à vos besoins, veuillez remplir le formulaire ci-dessous. Plus vous nous fournirez d'informations, plus nous serons en mesure de vous proposer un devis précis et adapté à vos besoins
-
-                                        </p>
-
-                                    </div>
-                                    {/*/.booking-working-hours*/}
-                                </div>
-                                {/*/.wrapper-booking*/}
-                            </section>
+                            <Devis/>
                             {/* ====== End Booking ====== */}
                         </main>
                     </div>
---
+++ components/Devis.tsx
@@ -0,0 +1,57 @@
+import { prefix } from '@/prefix';
+
+const Devis = () => {
+    return (
+        <section id="booking" className="ptb">
+        <div className="container">
+            {/* <span className="suptitle">Booking</span> */}
+            <h2>Devis</h2>
+        </div>
+        <div className="container wrapper-booking flex-row">
+            <div className="bookong-form">
+                <form method="POST">
+                    <div className="wrapper-input flex-row">
+                        <input type="text" name="name" placeholder="Prénom" />
+                        <input
+                            type="text"
+                            name="lastname"
+                            placeholder="Nom"
+                        />
+                    </div>
+                    {/*/.wrapper-input*/}
+                    <div className="wrapper-input flex-row">
+                        <input type="text" name="phone" placeholder="Numéro de téléphone" />
+                        <input type="text" name="email" placeholder="Adresse email" />
+                    </div>
+                    {/*/.wrapper-input*/}
+                    <div className="wrapper-input flex-row">
+                        <input type="text" name="date" placeholder="Ville d'exercice" />
+                        <input type="text" name="time" placeholder="Votre spécialité paramédicale" />
+                    </div>
+                    {/*/.wrapper-input*/}
+                    <div className="wrapper-input flex-row">
+                        <input type="text" name="date" placeholder="Message" className="large-input" />
+                    </div>
+                    <button className="btn" id="sendbook" type="submit">
+                        Obtenez  un devis gratuitement
+                    </button>
+                    <p className="res-booking" />
+                </form>
+            </div>
+            {/*/.bookong-form*/}
+            <div className="booking-working-hours">
+                <h3>Nous sommes heureux de vous proposer un devis gratuit pour nos services.</h3>
+                <p>
+                    Afin de mieux répondre à vos besoins, veuillez remplir le formulaire ci-dessous. Plus vous nous fournirez d'informations, plus nous serons en mesure de vous proposer un devis précis et adapté à vos besoins
+
+                </p>
+
+            </div>
+            {/*/.booking-working-hours*/}
+        </div>
+        {/*/.wrapper-booking*/}
+    </section>
+    );
+}
+
+export default Devis;
