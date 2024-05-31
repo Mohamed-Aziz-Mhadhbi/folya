455dd1977eb54ede59d14a06ede9f94021780180
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-05-31 16:13:08 +0100

    update

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -11,17 +11,20 @@ const Offers = () => {
                         Notre solution vous permet de gérer efficacement votre portefeuille patient, du planning des rendez-vous à la facturation en passant par la gestion de vos bilans.
                         Découvrez comment notre application peut transformer votre pratique quotidienne et améliorer votre vie professionnelle.
                     </p>
-
-                    <ul>
-                        <li><a className="btn appointment" href="#booking">
-                            Réservez votre démo
-                        </a></li>
-                        <li>
-                            <a href="https://www.youtube.com/watch?v=9_nyG2TUDcQ" className="play-btn mfp-iframe">
-                                <img className="wave1" src={`${prefix}/play-btn.png`} alt="Play Button" />
+                    <div className="hero-btn">
+                        <ul>
+                            <li><a className="btn appointment" href="#booking">
+                                Réservez votre démo
                             </a>
-                        </li>
-                    </ul>
+                            </li>
+                            <li>
+                                <a href="https://www.youtube.com/watch?v=9_nyG2TUDcQ" className="play-btn mfp-iframe">
+                                    <img className="wave1" src={`${prefix}/play-btn.png`} alt="play-btn" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
                 </div>
                 <div className="offer-img">
                     <img src={`${prefix}/img-header.png`} alt="Offer Image" />
