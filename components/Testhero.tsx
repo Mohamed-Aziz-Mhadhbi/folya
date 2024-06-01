a642c8d68cd1f3334c5f888ce5f42eaadc5fd64b
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-06-01 18:13:32 +0100

    update hero

--- components/Testhero.tsx
+++ components/Testhero.tsx
@@ -2,7 +2,36 @@ import { prefix } from '@/prefix';
 
 const Testhero = () => {
     return (
-        <section id="offer" className="hero-area">
+
+        <section className="hero-area">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-7 hero-col col-sm-12">
+                        <div className="hero-content">
+                            <h1>Best Outcome for <span>Every Patient !!!</span></h1>
+                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using.</p>
+                            <div className="hero-btn">
+                                <ul>
+                                    <li><a href="#" className="btn1">Get Started</a></li>
+                                    <li>
+                                        <a href="https://www.youtube.com/watch?v=9_nyG2TUDcQ" className="play-btn mfp-iframe">
+                                            <img className="wave1" src="images/play-btn.png" alt="play-btn" />
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    );
+};
+
+export default Testhero;
+
+{/* <section id="offer" className="hero-area">
             <div className="container">
                 <div className='row'>
                     <div className='col-lg-7 hero-col col-sm-12'>
@@ -28,14 +57,11 @@ const Testhero = () => {
                                 </ul>
                             </div>
                         </div>
-                        {/* <div className="offer-img">
+                        <div className="offer-img">
                             <img src={`${prefix}/img-header.png`} alt="Offer Image" />
-                        </div> */}
+                        </div> 
                     </div>
                 </div>
             </div>
-        </section>
-    );
-};
+        </section> */}
 
-export default Testhero;
