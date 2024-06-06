e4f1fadcc06d70ea1fd324c9bf8434c7bdf8f23b
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-06-06 09:57:39 +0100

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,42 +3,6 @@ import { prefix } from '@/prefix';
 const Navbar = () => {
     return (
         <header>
-            <div className="wrapper-top-header">
-                <div className="header-top container flex-row">
-                    <div className="header-contact flex-row">
-                        <p>
-                            <i className="fas fa-map-marker-alt" />
-                            Cité El Wafa - Nabeul 8062
-                        </p>
-                        <a href="#">
-                            <i className="fas fa-phone" />
-                            +216 27 405 988
-                        </a>
-                    </div>
-                    <ul className="social-link flex-row">
-                        <li>
-                            <a href="#">
-                                <i className="fab fa-facebook-f" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fab fa-twitter" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fab fa-instagram" />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fab fa-youtube" />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
             <div className="wrapper-bottom-header fixedmenu">
                 <div className="bottom-header container flex-row">
                     <a id="logo" href="#home">
