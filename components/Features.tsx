3eb17c1654c97144cfc7ed8986bc2c6b02d6d7a9
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-06-14 10:34:08 +0100

    update

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,46 +1,46 @@
 import { prefix } from '@/prefix';
 
-const Feature = () => {
+const Avantages = () => {
     return (
-        <section id="aboutus" className="ptb">
+        <section id="avantages" className="ptb">
             <div className="wrapper-title container align-center">
-                    <span className="suptitle">Avantages</span>
-                    <h2>Avantages</h2>
+                <span className="suptitle">Avantages</span>
+                <h2>Avantages</h2>
+            </div>
+            <div className="wrapper-feature container grid-container">
+                <div className="single-feature">
+                    <img src={`${prefix}/image/feature-1.png`} alt="Digitalisation de votre travail" />
+                    <h3>Digitalisation de votre travail et gain d’espace dans votre cabinet</h3>
+                    <p>Folya vous permet de numériser tous vos dossiers papiers, de libérer de l’espace dans votre cabinet et d’automatiser l'accès à vos fiches.</p>
                 </div>
-                <div className="wrapper-feature container flex-row">
-                    <div className="single-feature">
-                        <img src={`${prefix}/image/feature-1.png`} alt="Digitalisation de votre travail" />
-                        <h3>Digitalisation de votre travail et gain d’espace dans votre cabinet</h3>
-                        <p>Folya vous permet de numériser tous vos dossiers papiers, de libérer de l’espace dans votre cabinet et d’automatiser l'accès à vos fiches.</p>
-                    </div>
-                    <div className="single-feature">
-                        <img src={`${prefix}/image/feature-2.png`} alt="Organisation optimisée" />
-                        <h3>Organisation optimisée et gain du temps</h3>
-                        <p>Gérez tous les aspects de votre pratique à partir d'une seule plateforme, accessible d'un simple clic, où que vous soyez et à tout moment.</p>
-                    </div>
-                    <div className="single-feature">
-                        <img src={`${prefix}/image/feature-3.png`} alt="Amélioration de la gestion des données" />
-                        <h3>Amélioration de la gestion des données patient</h3>
-                        <p>Saisissez des informations importantes en temps réel, améliorez la coordination des données des patients et réduisez les risques d'erreurs médicales.</p>
-                    </div>
-                    <div className="single-feature">
-                        <img src={`${prefix}/image/feature-4.png`} alt="Dossiers patients centralisés" />
-                        <h3>Dossiers patients centralisés et sécurisés</h3>
-                        <p>Accédez aux dossiers patients, suivez l'évolution de l'état de vos patients, prenez des notes cliniques et stockez des informations en toute sécurité.</p>
-                    </div>
-                    <div className="single-feature">
-                        <img src={`${prefix}/image/feature-4.png`} alt="Facturation automatisée" />
-                        <h3>Facturation automatisée et suivi des finances</h3>
-                        <p>Dites adieu aux factures manuelles et aux erreurs de saisie ! Folya automatise la facturation et le suivi des paiements, tout en optimisant votre trésorerie.</p>
-                    </div>
-                    <div className="single-feature">
-                        <img src={`${prefix}/image/feature-3.png`} alt="Rapports et analyses" />
-                        <h3>Rapports et analyses précieuses</h3>
-                        <p>Obtenez des informations clés sur votre pratique et l’activité de votre cabinet grâce à des rapports et analyses personnalisés. Anticipez vos périodes de congé et prenez des décisions éclairées pour améliorer votre efficacité.</p>
-                    </div>
+                <div className="single-feature">
+                    <img src={`${prefix}/image/feature-2.png`} alt="Organisation optimisée" />
+                    <h3>Organisation optimisée et gain du temps</h3>
+                    <p>Gérez tous les aspects de votre pratique à partir d'une seule plateforme, accessible d'un simple clic, où que vous soyez et à tout moment.</p>
                 </div>
+                <div className="single-feature">
+                    <img src={`${prefix}/image/feature-3.png`} alt="Amélioration de la gestion des données" />
+                    <h3>Amélioration de la gestion des données patient</h3>
+                    <p>Saisissez des informations importantes en temps réel, améliorez la coordination des données des patients et réduisez les risques d'erreurs médicales.</p>
+                </div>
+                <div className="single-feature">
+                    <img src={`${prefix}/image/feature-4.png`} alt="Dossiers patients centralisés" />
+                    <h3>Dossiers patients centralisés et sécurisés</h3>
+                    <p>Accédez aux dossiers patients, suivez l'évolution de l'état de vos patients, prenez des notes cliniques et stockez des informations en toute sécurité.</p>
+                </div>
+                <div className="single-feature">
+                    <img src={`${prefix}/image/feature-4.png`} alt="Facturation automatisée" />
+                    <h3>Facturation automatisée et suivi des finances</h3>
+                    <p>Dites adieu aux factures manuelles et aux erreurs de saisie ! Folya automatise la facturation et le suivi des paiements, tout en optimisant votre trésorerie.</p>
+                </div>
+                <div className="single-feature">
+                    <img src={`${prefix}/image/feature-3.png`} alt="Rapports et analyses" />
+                    <h3>Rapports et analyses précieuses</h3>
+                    <p>Obtenez des informations clés sur votre pratique et l’activité de votre cabinet grâce à des rapports et analyses personnalisés. Anticipez vos périodes de congé et prenez des décisions éclairées pour améliorer votre efficacité.</p>
+                </div>
+            </div>
         </section>
     );
 }
 
-export default Feature;
+export default Avantages;
