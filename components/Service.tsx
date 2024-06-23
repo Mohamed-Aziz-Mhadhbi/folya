4defc9a41b1155bb0c764c95fd23e7ebe5e33f6f
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-06-23 10:58:40 +0100

    update

--- components/Service.tsx
+++ components/Service.tsx
@@ -12,7 +12,7 @@ const Service = () => {
                 
                 
                 <article className="align-center">
-                    <img src={`${prefix}/image/service-3.png`} alt="" />
+                    <img src={`${prefix}/image/suivi.png`} alt="" />
                     <h3>Suivi des patients</h3>
                     <p>
                         Gardez un œil sur l'évolution de l'état de vos patients et conservez un historique de votre prise en charge, depuis l'évaluation initiale jusqu'au compte rendu final, en incluant vos objectifs thérapeutiques.
@@ -20,7 +20,7 @@ const Service = () => {
                 </article>
 
                 <article className="align-center">
-                    <img src={`${prefix}/image/service-1.png`} alt="" />
+                    <img src={`${prefix}/image/rendez-vous.png`} alt="" />
                     <h3>Gestion des rendez-vous</h3>
                     <p>
                         Planifiez, modifiez et annulez vos rendez-vous en quelques clics en consultant votre calendrier.
@@ -28,7 +28,7 @@ const Service = () => {
                 </article>
 
                 <article className="align-center">
-                    <img src={`${prefix}/image/service-6.png`} alt="" />
+                    <img src={`${prefix}/image/analyses.png`} alt="" />
                     <h3>Rapports et analyses</h3>
                     <p>
                         Accédez à des informations clés sur votre pratique grâce à des rapports personnalisés.  Identifiez les tendances et optimisez vos processus, votre trésorerie, vos dépenses et votre comptabilité.
@@ -50,9 +50,9 @@ const Service = () => {
                         Automatisez la création, la consultation et l'envoi des factures, suivez les paiements en temps réel et obtenez des rapports détaillés sur vos revenus.
                     </p>
                 </article>
-               
+                
                 <article className="align-center">
-                    <img src={`${prefix}/image/service-2.png`} alt="" />
+                    <img src={`${prefix}/image/dossiersPatients.png`} alt="" />
                     <h3>Dossier patients paramédical standardisé et complet</h3>
                     <p>
                         Enregistrez et consultez les fiches administratives et médicales, effectuez des évaluations spécifiques à votre profession et conservez les ordonnances de prise en charge.
