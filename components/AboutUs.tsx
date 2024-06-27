72f16d833b88cb3f140fd25210c03f7adfda8897
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-06-27 11:20:08 +0100

    update aboutus

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -2,7 +2,7 @@ import { prefix } from '@/prefix';
 
 const AboutUs = () => {
     return (
-        <section id="aboutus" className="ptb flex-row">
+        <section id="aboutus" className="ptb">
             <div className="wrapper-aboutus container flex-row">
                 <div className="aboutus-img">
                     <img src={`${prefix}/image/aboutus.png`} alt="" />
