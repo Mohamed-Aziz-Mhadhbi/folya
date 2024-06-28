085864490f4c4096e1829bebaa91419f478e47fb
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-06-28 12:42:20 +0100

    change navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,56 +11,143 @@ const Navbar = () => {
 
     return (
         <header>
-            <div className="wrapper-bottom-header fixedmenu">
-                <div className="bottom-header container">
+            <div className="wrapper-top-header">
+                <div className="header-top container flex-row">
+                    <div className="header-contact flex-row">
+                        <p>
+                            <i className="fas fa-map-marker-alt" />
+                            2900 Lapeer Rd, Port Hurons, MI 49070
+                        </p>
+                        <a href="#">
+                            <i className="fas fa-phone" />
+                            +1 (800) 478-42-51
+                        </a>
+                    </div>
+                    {/*/.header-contact*/}
+                    <ul className="social-link">
+                        <li>
+                            <a href="#">
+                                <i className="fab fa-facebook-f" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fab fa-twitter" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fab fa-instagram" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fab fa-youtube" />
+                            </a>
+                        </li>
+                    </ul>
+                    {/*/.social-link*/}
+                </div>
+                {/*/.header-top*/}
+            </div>
+            {/*/.wrapper-top-header*/}
+            <div className="wrapper-bottom-header">
+                <div className="bottom-header container flex-row">
                     <a id="logo" href="#home">
-                        <img src={`${prefix}/image/logo.png`} alt="Logo" className="logo" />
+                        <img src="images/logo.png" alt="" />
                     </a>
-                    <nav id="nav" className={menuOpen ? 'open' : ''}>
-                        <a id="closemenu" className="btn close-btn" href="#" onClick={toggleMenu}>
+                    <nav id="nav">
+                        <a id="closemenu" className="btn" href="#">
                             <i className="fa-solid fa-xmark" />
                         </a>
                         <ul className="menu">
                             <li>
-                                <a href="#acceuil" onClick={toggleMenu}>
-                                    ACCEUIL
-                                </a>
+                                <a href="#home">Home</a>
                             </li>
                             <li>
-                                <a href="#a-propos" onClick={toggleMenu}>
-                                    A PROPOS
-                                </a>
+                                <a href="#aboutus">About Us</a>
                             </li>
                             <li>
-                                <a href="#avantages" onClick={toggleMenu}>
-                                    AVANTAGES
-                                </a>
-                            </li>
-                            <li >
-                                <a href="#fonctionalites" onClick={toggleMenu}>
-                                    FONCTIONALITES
-                                </a>
-                            </li>
-                            <li >
-                                <a href="#faq" onClick={toggleMenu}>
-                                    FAQ
-                                </a>
+                                <a href="#services">Services</a>
                             </li>
                             <li>
-                            <div className="menu-buttons">
-                                <a className="btn" href="#booking">Contact</a>
-                                <a className="btn" href="#">Connexion</a>
-                                <a className="btn" href="#">Devis</a>
-                            </div>
+                                <a href="#team">Team</a>
+                            </li>
+                            <li>
+                                <a href="#faq">FAQ</a>
+                            </li>
+                            <li>
+                                <a href="#pricing">Pricing</a>
+                            </li>
+                            <li>
+                                <a href="#contacts">Contact</a>
                             </li>
                         </ul>
                     </nav>
-                    <a className="btn menu-toggle-btn" href="#" onClick={toggleMenu}>
+                    {/*#nav*/}
+                    <a className="btn appointment" href="#booking">
+                        Appointment
+                    </a>
+                    <a id="openmenu" className="btn" href="#">
                         <i className="fa-solid fa-bars" />
                     </a>
                 </div>
+                {/*/.bottom-header*/}
             </div>
+            {/*/.wrapper-bottom-header*/}
         </header>
+
+        // <header>
+        //     <div className="wrapper-bottom-header fixedmenu">
+        //         <div className="bottom-header container">
+        //             <a id="logo" href="#home">
+        //                 <img src={`${prefix}/image/logo.png`} alt="Logo" className="logo" />
+        //             </a>
+        //             <nav id="nav" className={menuOpen ? 'open' : ''}>
+        //                 <a id="closemenu" className="btn close-btn" href="#" onClick={toggleMenu}>
+        //                     <i className="fa-solid fa-xmark" />
+        //                 </a>
+        //                 <ul className="menu">
+        //                     <li>
+        //                         <a href="#acceuil" onClick={toggleMenu}>
+        //                             ACCEUIL
+        //                         </a>
+        //                     </li>
+        //                     <li>
+        //                         <a href="#a-propos" onClick={toggleMenu}>
+        //                             A PROPOS
+        //                         </a>
+        //                     </li>
+        //                     <li>
+        //                         <a href="#avantages" onClick={toggleMenu}>
+        //                             AVANTAGES
+        //                         </a>
+        //                     </li>
+        //                     <li >
+        //                         <a href="#fonctionalites" onClick={toggleMenu}>
+        //                             FONCTIONALITES
+        //                         </a>
+        //                     </li>
+        //                     <li >
+        //                         <a href="#faq" onClick={toggleMenu}>
+        //                             FAQ
+        //                         </a>
+        //                     </li>
+        //                     <li>
+        //                     <div className="menu-buttons">
+        //                         <a className="btn" href="#booking">Contact</a>
+        //                         <a className="btn" href="#">Connexion</a>
+        //                         <a className="btn" href="#">Devis</a>
+        //                     </div>
+        //                     </li>
+        //                 </ul>
+        //             </nav>
+        //             <a className="btn menu-toggle-btn" href="#" onClick={toggleMenu}>
+        //                 <i className="fa-solid fa-bars" />
+        //             </a>
+        //         </div>
+        //     </div>
+        // </header>
     );
 }
 
