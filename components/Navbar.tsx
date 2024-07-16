a53256158ef13210e5c93bea03f373fe478f7d90
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-07-16 13:45:00 +0100

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = () => {
                     <a className="btn appointment" href="#contacts">
                         Contact
                     </a>
-                    <a className="btn appointment" href="#connexion">
+                    <a className="btn appointment" href="https://www.folya.co/login">
                         Connexion
                     </a>
                     <a className="btn appointment" href="#booking">
