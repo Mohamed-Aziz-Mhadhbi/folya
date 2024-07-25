6232f9e9aeabb2ea728475b51eafaca3d62140d1
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-07-25 11:11:00 +0100

    test navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,20 @@ import { prefix } from '@/prefix';
 const Navbar = () => {
     return (
         <header>
+            <div className="wrapper-top-header">
+                <div className="header-top container flex-row">
+                    <div className="header-contact flex-row">
+                        <p><i className="fas fa-map-marker-alt"></i>2900 Lapeer Rd, Port Hurons, MI 49070</p>
+                        <a href="#"><i className="fas fa-phone"></i>+1 (800) 478-42-51</a>
+                    </div>
+                    <ul className="social-link">
+                        <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                        <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i className="fab fa-youtube"></i></a></li>
+                    </ul>
+                </div>
+            </div>
             <div className="wrapper-bottom-header">
                 <div className="bottom-header container flex-row">
                     <a id="logo" href="#home">
@@ -32,15 +46,15 @@ const Navbar = () => {
                         </ul>
                     </nav>
                     <nav>
-                    <a className="btn appointment" href="#contacts">
-                        Contact
-                    </a>
-                    <a className="btn appointment" href="https://www.folya.co/login">
-                        Connexion
-                    </a>
-                    <a className="btn appointment" href="#booking">
-                        Démo
-                    </a>
+                        <a className="btn appointment" href="#contacts">
+                            Contact
+                        </a>
+                        <a className="btn appointment" href="https://www.folya.co/login">
+                            Connexion
+                        </a>
+                        <a className="btn appointment" href="#booking">
+                            Démo
+                        </a>
                     </nav>
                     <a id="openmenu" className="btn" href="#">
                         <i className="fa-solid fa-bars" />
