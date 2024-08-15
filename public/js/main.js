d93b6f9f32d4a3b48eabf09e748175d7d0c00d26
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-08-15 12:18:23 +0100

    full main

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
-
- $(document).ready(function () {
+$(document).ready(function () {
 
     $('.client-single').on('click', function (event) {
         event.preventDefault();
@@ -23,5 +22,91 @@
 
         }
     });
+
+    // scroll top
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll < 500) {
+            $(".bottomToup").removeClass("active-top");
+        } else {
+            $(".bottomToup").addClass("active-top");
+        }
+    });
+
+    // preloader
+    jQuery(window).load(function () {
+        $(".loader").fadeOut(1000);
+    });
+
+
+    $('.navigation-container').sticky({
+        topSpacing: 0
+    });
+
+
+    $('#menu').slicknav({
+        prependTo: '.header-area .navigation-container .main-menu',
+        closeOnClick: true, // Close menu when a link is clicked.
+    });
+
+
+    $('.play-btn').magnificPopup({
+        type: 'video'
+    })
+
+    var mixer = mixitup('.doctor-list');
+
+
+    $("#datepicker").datepicker();
+
+    const target = document.querySelector(".target");
+    const links = document.querySelectorAll(".mynav .single-services-item");
+    const colors = [""];
+
+    function mouseenterFunc() {
+        if (!this.parentNode.classList.contains("active")) {
+            for (let i = 0; i < links.length; i++) {
+                if (links[i].parentNode.classList.contains("active")) {
+                    links[i].parentNode.classList.remove("active");
+                }
+                links[i].style.opacity = "1";
+            }
+
+            this.parentNode.classList.add("active");
+            this.style.opacity = "1";
+
+            const width = this.getBoundingClientRect().width;
+            const height = this.getBoundingClientRect().height;
+            const left = this.getBoundingClientRect().left + window.pageXOffset;
+            const top = this.getBoundingClientRect().top + window.pageYOffset;
+            const color = colors[Math.floor(Math.random() * colors.length)];
+
+            target.style.width = `${width}px`;
+            target.style.height = `${height}px`;
+            target.style.left = `${left}px`;
+            target.style.top = `${top}px`;
+            target.style.transform = "none";
+        }
+    }
+
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", (e) => e.preventDefault());
+        links[i].addEventListener("mouseenter", mouseenterFunc);
+    }
+
+    function resizeFunc() {
+        const active = document.querySelector(".mynav .services-col.active");
+
+        if (active) {
+            const left = active.getBoundingClientRect().left + window.pageXOffset;
+            const top = active.getBoundingClientRect().top + window.pageYOffset;
+
+            target.style.left = `${left}px`;
+            target.style.top = `${top}px`;
+        }
+    }
+
+    window.addEventListener("resize", resizeFunc);
+
 
 }(jQuery));
