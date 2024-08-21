add748b7a4f15108668834a152f69c0c1c2bc2eb
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-08-21 13:21:58 +0100

    adding felx-row to avantages

--- components/Features.tsx
+++ components/Features.tsx
@@ -8,7 +8,7 @@ const Feature = () => {
                 <h2>Avantages</h2>
             </div>
 
-            <div className="wrapper-pricing container">
+            <div className="wrapper-pricing container flex-row">
                 <div className="flex-row">
                     <div className="single-pricing align-center">
                         <div className="pricing-title">
@@ -41,37 +41,36 @@ const Feature = () => {
                     </div>
                 </div>
 
-                <div className="flex-row">
-                    <div className="single-pricing align-center">
-                        <div className="pricing-title">
-                            <img src={`${prefix}/image/dossiersPatients.png`} alt="Dossiers patients centralisés" />
-                            <h3>Dossiers patients centralisés et sécurisés</h3>
-                        </div>
-                        <div className="pricing-content">
-                            <p>Accédez aux dossiers patients, suivez l'évolution de l'état de vos patients, prenez des notes cliniques et stockez des informations en toute sécurité.</p>
-                        </div>
+                <div className="single-pricing align-center">
+                    <div className="pricing-title">
+                        <img src={`${prefix}/image/dossiersPatients.png`} alt="Dossiers patients centralisés" />
+                        <h3>Dossiers patients centralisés et sécurisés</h3>
                     </div>
-
-                    <div className="single-pricing align-center">
-                        <div className="pricing-title">
-                            <img src={`${prefix}/image/suivi.png`} alt="Facturation automatisée" />
-                            <h3>Facturation automatisée et suivi des finances</h3>
-                        </div>
-                        <div className="pricing-content">
-                            <p>Dites adieu aux factures manuelles et aux erreurs de saisie ! Folya automatise la facturation et le suivi des paiements, tout en optimisant votre trésorerie.</p>
-                        </div>
-                    </div>
-
-                    <div className="single-pricing align-center">
-                        <div className="pricing-title">
-                            <img src={`${prefix}/image/analyses.png`} alt="Rapports et analyses" />
-                            <h3>Rapports et analyses précieuses</h3>
-                        </div>
-                        <div className="pricing-content">
-                            <p>Obtenez des informations clés sur votre pratique et l’activité de votre cabinet grâce à des rapports et analyses personnalisés. Anticipez vos périodes de congé et prenez des décisions éclairées pour améliorer votre efficacité.</p>
-                        </div>
+                    <div className="pricing-content">
+                        <p>Accédez aux dossiers patients, suivez l'évolution de l'état de vos patients, prenez des notes cliniques et stockez des informations en toute sécurité.</p>
                     </div>
                 </div>
+
+                <div className="single-pricing align-center">
+                    <div className="pricing-title">
+                        <img src={`${prefix}/image/suivi.png`} alt="Facturation automatisée" />
+                        <h3>Facturation automatisée et suivi des finances</h3>
+                    </div>
+                    <div className="pricing-content">
+                        <p>Dites adieu aux factures manuelles et aux erreurs de saisie ! Folya automatise la facturation et le suivi des paiements, tout en optimisant votre trésorerie.</p>
+                    </div>
+                </div>
+
+                <div className="single-pricing align-center">
+                    <div className="pricing-title">
+                        <img src={`${prefix}/image/analyses.png`} alt="Rapports et analyses" />
+                        <h3>Rapports et analyses précieuses</h3>
+                    </div>
+                    <div className="pricing-content">
+                        <p>Obtenez des informations clés sur votre pratique et l’activité de votre cabinet grâce à des rapports et analyses personnalisés. Anticipez vos périodes de congé et prenez des décisions éclairées pour améliorer votre efficacité.</p>
+                    </div>
+                </div>
+
             </div>
         </section>
     );
