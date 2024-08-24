156bf961aadb8d96433e2f4ce124470cad060ff3
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-08-24 15:19:40 +0100

    test

--- public/js/main.js
+++ public/js/main.js
@@ -56,6 +56,56 @@ $(document).ready(function () {
 
     var mixer = mixitup('.doctor-list');
 
+
+    $("#datepicker").datepicker();
+
+    const target = document.querySelector(".target");
+    const links = document.querySelectorAll(".mynav .single-services-item");
+    const colors = [""];
+
+    function mouseenterFunc() {
+        if (!this.parentNode.classList.contains("active")) {
+            for (let i = 0; i < links.length; i++) {
+                if (links[i].parentNode.classList.contains("active")) {
+                    links[i].parentNode.classList.remove("active");
+                }
+                links[i].style.opacity = "1";
+            }
+
+            this.parentNode.classList.add("active");
+            this.style.opacity = "1";
+
+            const width = this.getBoundingClientRect().width;
+            const height = this.getBoundingClientRect().height;
+            const left = this.getBoundingClientRect().left + window.pageXOffset;
+            const top = this.getBoundingClientRect().top + window.pageYOffset;
+            const color = colors[Math.floor(Math.random() * colors.length)];
+
+            target.style.width = `${width}px`;
+            target.style.height = `${height}px`;
+            target.style.left = `${left}px`;
+            target.style.top = `${top}px`;
+            target.style.transform = "none";
+        }
+    }
+
+    for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", (e) => e.preventDefault());
+        links[i].addEventListener("mouseenter", mouseenterFunc);
+    }
+
+    function resizeFunc() {
+        const active = document.querySelector(".mynav .services-col.active");
+
+        if (active) {
+            const left = active.getBoundingClientRect().left + window.pageXOffset;
+            const top = active.getBoundingClientRect().top + window.pageYOffset;
+
+            target.style.left = `${left}px`;
+            target.style.top = `${top}px`;
+        }
+    }
+
     window.addEventListener("resize", resizeFunc);
 
 
