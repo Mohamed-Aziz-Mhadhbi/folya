0f89bc4bd66cb6afc4e14e73b9a69dfc4bb90e47
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-09-06 12:26:07 +0100

    next Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: '/my-nextjs-app',
+    // basePath: '/my-nextjs-app',
     assetPrefix: '/my-nextjs-app',
     // trailingSlash: true,
 };
