e2ad253b9a02be9cf85da252a0e1955425c5afb4
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-09-10 09:44:39 +0100

    update

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -4,8 +4,8 @@ const Offers = () => {
             <div className="wrapper-offer container flex-row">
                 <div className="offer-content">
                     <h2>
-                        FOLYA,
-                        <span className="selection"> Votre solution de gestion de cabinet paramédical tout en un, clé en main.</span>
+                    <span className="selection">  FOLYA,</span>
+                        votre solution de gestion de cabinet paramédical tout en un, clé en main.
                     </h2>
                     <p>
                         Développée par des experts du secteur paramédical, notre solution vous offre une gestion optimale de votre portefeuille de patients, allant de la planification des rendez-vous à la facturation, tout en intégrant le suivi de vos bilans.
