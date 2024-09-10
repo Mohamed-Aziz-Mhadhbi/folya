04c3998fbd982e26605e8802656855ffd1ec0d21
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-09-10 11:31:46 +0100

    update AVANTAGE

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -8,13 +8,13 @@ const Offers = () => {
                     votre solution tout-en-un, clé en main, pour simplifier la gestion de votre cabinet paramédical.
                     </h2>
                     <p>
-                    Transformez la gestion de votre cabinet avec FOLYA, conçue par des experts du paramédical ! Centralisez vos dossiers patients en un clic, gagnez un temps précieux et concentrez-vous sur l'essentiel : vos patients. Adoptez FOLYA dès aujourd'hui et boostez votre efficacité !   
+                    Transformez la gestion de votre cabinet avec FOLYA, conçue par des experts du paramédical ! Centralisez vos dossiers patients en un clic, gagnez un temps précieux et concentrez-vous sur l'essentiel : vos patients. &nbsp;Adoptez FOLYA dès aujourd'hui et boostez votre efficacité !   
                     </p>
                     <div className="hero-btn">
                         <ul>
                             <li>
                                 <a className="btn appointment animated-btn" href="#booking">
-                                    Réservez votre démo
+                                demandez votre devis
                                 </a>
                             </li>
                         </ul>
