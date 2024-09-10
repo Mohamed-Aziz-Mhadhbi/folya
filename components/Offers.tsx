bd0b36d85b04051d87f2b80dfc5e3e6a2c4c3ff6
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-09-10 12:00:04 +0100

    update AVANTAGE

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -3,20 +3,21 @@ const Offers = () => {
         <section id="offer" className="ptb">
             <div className="wrapper-offer container flex-row">
                 <div className="offer-content">
-                    <h2>
-                    <span className="selection">  Folya,</span>
-                    votre solution clé en main, pour la gestion de votre cabinet paramédical.
+                    <h2 className="offer-heading">
+                        <span className="selection">Folya,</span>
+                        votre solution clé en main, pour la gestion de votre cabinet paramédical.
                     </h2>
                     <p>
-                    Transformez la gestion de votre cabinet avec FOLYA, conçue par des experts du paramédical ! Centralisez vos dossiers patients en un clic, gagnez un temps précieux et concentrez-vous sur l'essentiel : vos patients. </p>
+                        Transformez la gestion de votre cabinet avec FOLYA, conçue par des experts du paramédical ! Centralisez vos dossiers patients en un clic, gagnez un temps précieux et concentrez-vous sur l'essentiel : vos patients.
+                    </p>
                     <p>
-                    Adoptez FOLYA dès aujourd'hui et boostez votre efficacité !   
+                        Adoptez FOLYA dès aujourd'hui et boostez votre efficacité !
                     </p>
                     <div className="hero-btn">
                         <ul>
                             <li>
                                 <a className="btn appointment animated-btn" href="#booking">
-                                demandez votre devis
+                                    demandez votre devis
                                 </a>
                             </li>
                         </ul>
