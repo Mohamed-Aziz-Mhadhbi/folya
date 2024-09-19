6e8a7e58815df954fad42c7479947168319b5f5c
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-09-19 11:19:31 +0100

    nom text field

--- components/form/TextFieldNom.tsx
+++ components/form/TextFieldNom.tsx
@@ -1,4 +1,21 @@
+import { useState } from 'react';
+
 export default function TextFieldNom() {
+    const [lastName, setLastName] = useState('');
+    const [error, setError] = useState(''); 
+
+    const handleInputChange = (event:any) => {
+        setLastName(event.target.value);
+    };
+
+    const handleBlur = () => {
+        if (!lastName) {
+            setError('Nom is required'); 
+        } else {
+            setError(''); 
+        }
+    };
+
     return (
         <div className="field-wrapper" data-row={2} data-step="false">
             <input
@@ -7,8 +24,13 @@ export default function TextFieldNom() {
                 placeholder="Nom *"
                 autoComplete="off"
                 required
+                value={lastName}
+                onChange={handleInputChange}
+                onBlur={handleBlur}
+                className={error ? 'error' : 'valid'}
             />
             <label className="label-and-placeholder">Nom *</label>
+            {error && <div className="error">{error}</div>}
         </div>
-    )
-};
+    );
+}
