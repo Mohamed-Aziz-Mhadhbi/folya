38e72811484d24c0b35f43900d8d6266e939b4bc
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-09-19 11:26:13 +0100

    textfield prénom validation error

--- components/form/TextFieldPrenom.tsx
+++ components/form/TextFieldPrenom.tsx
@@ -1,4 +1,21 @@
+import { useState } from 'react';
+
 export default function TextFieldPrenom() {
+    const [firstName, setFirstName] = useState('');
+    const [error, setError] = useState(''); 
+
+    const handleInputChange = (event:any) => {
+        setFirstName(event.target.value);
+    };
+
+    const handleBlur = () => {
+        if (!firstName) {
+            setError('Veuillez saisir votre prénom'); 
+        } else {
+            setError(''); 
+        }
+    };
+
     return (
         <div className="field-wrapper" data-row={2} data-step="false">
             <input
@@ -7,8 +24,13 @@ export default function TextFieldPrenom() {
                 placeholder="Prénom *"
                 autoComplete="off"
                 required
+                value={firstName}
+                onChange={handleInputChange}
+                onBlur={handleBlur}
+                className={error ? 'error' : 'valid'}
             />
             <label className="label-and-placeholder">Prénom *</label>
+            {error && <div className="error">{error}</div>}
         </div>
-    )
-};
+    );
+}
