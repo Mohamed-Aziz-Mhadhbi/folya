7ef8b34172a9f769cacc709df744078888edcfbd
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-09-20 07:21:12 +0100

    update

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -2,24 +2,15 @@ const Offers = () => {
     return (
         <section id="offer" className="ptb">
             <div className="wrapper-offer container">
-                {/* Background video */}
-                <div className="background-video">
-                    <iframe
-                        width="100%"
-                        height="100%"
-                        src="https://www.youtube.com/embed/9_nyG2TUDcQ?autoplay=1&mute=1&loop=1"
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen>
-                    </iframe>
-                </div>
+                {/* Heading section */}
                 <div className="heading-section">
                     <h2 className="offer-heading">
                         <span className="selection">Folya,</span>
                         votre solution de gestion du cabinet paramédical.
                     </h2>
                 </div>
+                
+                {/* Text and video side by side */}
                 <div className="content-section flex-row">
                     <div className="offer-content">
                         <p>
@@ -37,6 +28,17 @@ const Offers = () => {
                                 </li>
                             </ul>
                         </div>
+                    </div>
+                    <div className="offer-video">
+                        <iframe
+                            width="560"
+                            height="315"
+                            src="https://www.youtube.com/embed/9_nyG2TUDcQ"
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen>
+                        </iframe>
                     </div>
                 </div>
             </div>
