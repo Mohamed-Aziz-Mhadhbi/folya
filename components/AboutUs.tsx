ef302b085bcec34e0d482672330f6d900852c0bc
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-10-17 15:12:54 +0100

    update

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -12,9 +12,9 @@ const AboutUs = () => {
                     <span className="suptitle">En Savoir Plus</span>
                     <h2>Au cœur de notre mission <span className="selection">se trouvent votre bien-être et celui de vos patients</span></h2>
                     <p>
-                        Imaginez votre quotidien libéré de la paperasse et des tâches chronophages.
-                        En tant que professionnel paramédical, votre dévouement contribue à sauver des vies chaque jour, mais la gestion administrative ne devrait pas vous ralentir.
-                    </p>
+                    Vous êtes kinésithérapeute, orthophoniste, ergothérapeute, psychologue ou psychomotricien ?
+Nous comprenons les défis spécifiques de votre profession et l&#39;impact que les tâches
+administratives peuvent avoir sur votre pratique.   </p>
                     <p>Folya est votre allié, conçu par des professionnels paramédicaux pour vous simplifier la vie et vous permettre de relever les défis quotidiens liés à la gestion des patients. </p>
                     <p>Folya vous permet de centraliser et de gérer facilement tous les aspects de votre cabinet, où que vous soyez et quand vous le désirez. Dossiers patients, planification de rendez-vous, intégration des comptes rendus et suivi des finances... </p>
                     <p>Nous vous offrons une solution, clés en main, complète pour une organisation optimale.</p>
