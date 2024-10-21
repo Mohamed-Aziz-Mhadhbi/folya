9a12f13bc54dbb907aa65f8158ddca3145d3cec9
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-10-21 17:33:40 +0100

    update

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -17,7 +17,6 @@ const FAQ = () => {
                                 Folya est une application de gestion de cabinet paramédical complète conçue pour simplifier et optimiser votre pratique quotidienne. Elle vous permet de centraliser toutes vos tâches administratives et cliniques en un seul endroit, vous offrant ainsi une solution efficace pour améliorer votre productivité et la qualité des soins prodigués à vos patients.
                             </p>
                         </div>
-                        {/*/.content-accordion*/}
                         <h3>
                             <i className="fa-solid fa-chevron-down" />
                             Comment puis-je commencer à utiliser Folya ?
@@ -30,7 +29,6 @@ const FAQ = () => {
                                 Folya est simple et rapide. Vous pouvez vous connecter à votre application sur notre site web et suivre les instructions fournies pour créer votre compte. Notre équipe d'assistance est également disponible pour vous guider et répondre à toutes vos questions.
                             </p>
                         </div>
-                        {/*/.content-accordion*/}
                         <h3>
                             <i className="fa-solid fa-chevron-down" />
                             Folya est-elle compatible avec mon appareil ?
@@ -44,7 +42,6 @@ const FAQ = () => {
                                 La compatibilité de Folya vous assure une accessibilité flexible et sans contraintes, quel que soit l’appareil utilisé pour votre connexion.
                             </p>
                         </div>
-                        {/*/.content-accordion*/}
                         <h3>
                             <i className="fa-solid fa-chevron-down" />
                             Folya est-elle sécurisée ?
@@ -57,7 +54,6 @@ const FAQ = () => {
                                 La protection de vos données est notre principale préoccupation. Folya intègre des technologies de sécurité avancées pour stocker vos informations sensibles. Nous nous engageons à respecter les normes les plus strictes en matière de confidentialité et de sécurité des données de santé, en collaborant avec des hébergeurs de santé certifiés.
                             </p>
                         </div>
-                        {/*/.content-accordion*/}
                         <h3>
                             <i className="fa-solid fa-chevron-down" />
                             Puis-je essayer Folya gratuitement ?
@@ -69,7 +65,6 @@ const FAQ = () => {
                             <p>
                                 Oui, vous pouvez profiter d'un essai gratuit de 30 jours pour découvrir toutes les fonctionnalités de Folya et voir comment elle peut vous aider à améliorer votre pratique. Inscrivez-vous dès aujourd'hui pour commencer votre essai gratuit ! </p>
                         </div>
-                        {/*/.content-accordion*/}
                         <h3>
                             <i className="fa-solid fa-chevron-down" />
                             Comment puis-je obtenir de l'aide avec Folya ?
@@ -81,9 +76,7 @@ const FAQ = () => {
                             <p>
                                 Notre équipe d'assistance est disponible pour vous aider à tirer le meilleur parti de Folya. Vous pouvez nous contacter par e-mail, téléphone ou chat en direct. Nous sommes toujours heureux de répondre à vos questions et de vous fournir le support dont vous avez besoin. </p>
                         </div>
-                        {/*/.content-accordion*/}
                     </div>
-                    {/*wrapper-accordion*/}
                 </div>
                 <div className="faq-img">
                     <img src={`${prefix}/image/faq.png`} alt="" />
