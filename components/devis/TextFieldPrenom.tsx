9e99f2305d06c894cfed66cfbbf0b1e8bc4012a9
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-11-01 10:16:44 +0100

    update

--- components/devis/TextFieldPrenom.tsx
+++ components/devis/TextFieldPrenom.tsx
@@ -29,7 +29,7 @@ export default function TextFieldPrenom() {
                 onBlur={handleBlur}
                 className={error ? 'error' : 'valid'}
             />
-            <div className="error-message">{error && <div className="error error--none">{error}</div>}</div>
+            {error && <div className="error error--none">{error}</div>}
         </div>
     );
 }
