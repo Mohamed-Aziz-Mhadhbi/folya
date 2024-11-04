0b577e4709a9863ebf376050565eccf45f35e398
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-11-04 11:53:59 +0100

    mod

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -15,41 +15,45 @@ const AboutUs = () => {
                     <p>Nous comprenons les défis spécifiques de votre profession et l'impact que les tâches administratives peuvent avoir sur votre pratique.</p>
                     <p id='colorsp'>Folya est bien plus qu’une simple plateforme :  c'est votre allié.</p>
                     <p>Conçu par des professionnels paramédicaux pour vous simplifier la vie et vous permettre de relever les défis quotidiens liés à la gestion des patients et de votre cabinet, où que vous soyez et quand vous le désirez.  </p>
-                    <div className="wrapper-aboutus-progress">
-                        <div className="single-progress flex-row">
-                            <div className="progress-img">
-                                <img src={`${prefix}/image/reduc.png`} alt="" />
+                    <div className="wrapper-aboutus-progress flex-row">
+                        <div className="single-progress-wrapper flex-row">
+                            <div className="single-progress flex-row">
+                                <div className="progress-img">
+                                    <img src={`${prefix}/image/reduc.png`} alt="" />
+                                </div>
+                                <div className="progress-content">
+                                    <h3>Réduction des erreurs administratives</h3>
+                                    <p>20 à 30 % de diminution des erreurs liées à la gestion des dossiers.</p>
+                                </div>
                             </div>
-                            <div className="progress-content">
-                                <h3>Réduction des erreurs administratives</h3>
-                                <p>20 à 30 % de diminution des erreurs liées à la gestion des dossiers.</p>
-                            </div>
-                        </div>
-                        <div className="single-progress flex-row">
-                            <div className="progress-img">
-                                <img src={`${prefix}/image/improve.png`} alt="" />
-                            </div>
-                            <div className="progress-content">
-                                <h3>Amélioration de l'efficacité</h3>
-                                <p>80 % des premiers utilisateurs rapportent une meilleure organisation dès les premières semaines d'utilisation.</p>
-                            </div>
-                        </div>
-                        <div className="single-progress flex-row">
-                            <div className="progress-img">
-                                <img src={`${prefix}/image/rapide.png`} alt="" />
-                            </div>
-                            <div className="progress-content">
-                                <h3>Accès rapide aux informations</h3>
-                                <p>Moins de 30 secondes pour accéder aux dossiers patients avec Folya.</p>
+                            <div className="single-progress flex-row">
+                                <div className="progress-img">
+                                    <img src={`${prefix}/image/improve.png`} alt="" />
+                                </div>
+                                <div className="progress-content">
+                                    <h3>Amélioration de l'efficacité</h3>
+                                    <p>80 % des premiers utilisateurs rapportent une meilleure organisation dès les premières semaines d'utilisation.</p>
+                                </div>
                             </div>
                         </div>
-                        <div className="single-progress flex-row">
-                            <div className="progress-img">
-                                <img src={`${prefix}/image/gestionDonnees.png`} alt="" />
+                        <div className="single-progress-wrapper flex-row">
+                            <div className="single-progress flex-row">
+                                <div className="progress-img">
+                                    <img src={`${prefix}/image/rapide.png`} alt="" />
+                                </div>
+                                <div className="progress-content">
+                                    <h3>Accès rapide aux informations</h3>
+                                    <p>Moins de 30 secondes pour accéder aux dossiers patients avec Folya.</p>
+                                </div>
                             </div>
-                            <div className="progress-content">
-                                <h3>Réduction des coûts</h3>
-                                <p>Jusqu'à 45 % de baisse de vos dépenses administratives grâce à l'automatisation et l’optimisation de la gestion de votre cabinet .</p>
+                            <div className="single-progress flex-row">
+                                <div className="progress-img">
+                                    <img src={`${prefix}/image/gestionDonnees.png`} alt="" />
+                                </div>
+                                <div className="progress-content">
+                                    <h3>Réduction des coûts</h3>
+                                    <p>Jusqu'à 45 % de baisse de vos dépenses administratives grâce à l'automatisation et l’optimisation de la gestion de votre cabinet .</p>
+                                </div>
                             </div>
                         </div>
                     </div>
