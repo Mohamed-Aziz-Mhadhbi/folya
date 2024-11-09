2a138c24534c11ae4f4bef701489f0b086901fc6
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-11-09 12:53:32 +0100

    update

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -3,11 +3,9 @@ import { prefix } from "@/prefix";
 const Offers = () => {
     return (
         <section id="offer" className="ptb">
-            <div className="offer-img">
-                    <img src={`${prefix}/offers.png`} alt="" />
-                </div>
+
             <div className="wrapper-offer container flex-row">
-                {/* <div className="offer-content">
+                <div className="offer-content">
                     <h2 className="offer-heading">
                         <span className="selection">Folya,</span>
                         votre solution de gestion du cabinet paramédical.
@@ -18,9 +16,11 @@ const Offers = () => {
                     <p>
                         Adoptez FOLYA dès aujourd'hui et boostez votre efficacité !
                     </p><a className="btn appointment" href="#booking">Demandez votre devis</a>
-                </div> */}
+                </div>
             </div>
-            
+            <div className="offer-img">
+                <img src={`${prefix}/offers.png`} alt="" />
+            </div>
         </section>
     );
 };
