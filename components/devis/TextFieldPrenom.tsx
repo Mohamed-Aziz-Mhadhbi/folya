0ccbfa03aee0462bd36cc8f1d5863e779ecf745f
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-11-10 22:30:50 +0100

    devis

--- components/devis/TextFieldPrenom.tsx
+++ components/devis/TextFieldPrenom.tsx
@@ -17,7 +17,7 @@ export default function TextFieldPrenom() {
     };
 
     return (
-        <div className='fields'>
+        <>
             <input
                 type="text"
                 name="first_name"
@@ -30,6 +30,6 @@ export default function TextFieldPrenom() {
                 className={error ? 'error' : 'valid'}
             />
             {error && <label className="error error--none">{error}</label>}
-        </div>
+        </>
     );
 }
