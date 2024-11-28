457c5680972f4b8d587ddca2dbcbdd0838ca5f4c
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-11-28 15:34:58 +0100

    update

--- components/Tarif.tsx
+++ components/Tarif.tsx
@@ -7,70 +7,58 @@ const Tarif = () => {
                 <span className="suptitle">Tarifs</span>
                 <h2>Nous Offrons Les <span className="selection">Meilleurs Tarifs</span></h2>
             </div>
-
             <div className="wrapper-pricing container flex-row">
                 <div className="single-pricing align-center">
                     <div className="pricing-title">
                         <img src={`${prefix}/image/improve.png`} alt="" />
                         <h3>Tarif </h3>
                     </div>
-
                     <div className="pricing-content">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab perferendis, voluptatum magni
                             natus sunt dolorem quibusdam esse eaque repellendus?</p>
                     </div>
-
                     <div className="single-price">
                         <p>130€</p>
                         <span>Par Mois</span>
                     </div>
                 </div>
-
                 <div className="single-pricing align-center">
                     <div className="pricing-title">
                         <img src={`${prefix}/image/improve.png`} alt="" />
                         <h3>tarif</h3>
                     </div>
-
                     <div className="pricing-content">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab perferendis, voluptatum magni
                             natus sunt dolorem quibusdam esse eaque repellendus?</p>
                     </div>
-
                     <div className="single-price">
                         <p>240€</p>
                         <span>Par Mois</span>
                     </div>
                 </div>
-
                 <div className="single-pricing align-center">
                     <div className="pricing-title">
                         <img src={`${prefix}/image/improve.png`} alt="" />
                         <h3>tarif</h3>
                     </div>
-
                     <div className="pricing-content">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab perferendis, voluptatum magni
                             natus sunt dolorem quibusdam esse eaque repellendus?</p>
                     </div>
-
                     <div className="single-price">
                         <p>420€</p>
                         <span>Par Mois</span>
                     </div>
                 </div>
-
                 <div className="single-pricing align-center">
                     <div className="pricing-title">
                         <img src={`${prefix}/image/improve.png`} alt="" />
                         <h3>tarif</h3>
                     </div>
-
                     <div className="pricing-content">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ab perferendis, voluptatum magni
                             natus sunt dolorem quibusdam esse eaque repellendus?</p>
                     </div>
-
                     <div className="single-price">
                         <p>380€</p>
                         <span>Par Mois</span>
