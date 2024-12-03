70b76e09f543207d73e4ca7953c4d35821f4bbac
mohamed.aziz.mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2024-12-03 12:22:48 +0100

    udpdate

--- components/Service.tsx
+++ components/Service.tsx
@@ -9,11 +9,6 @@ const Service = () => {
             </div>
             <div className="wrapper-service container flex-row">
                 <article className="align-center">
-                    <img src={`${prefix}/image/dossiersPatients.png`} alt="" />
-                    <h3>Dossier-patient standardisé</h3>
-                    <p>Enregistrez et consultez les fiches administratives, médicales et les comptes-rendus. Conservez les prescriptions de soins. </p>
-                </article>
-                <article className="align-center">
                     <img src={`${prefix}/image/service-4.png`} alt="" />
                     <h3>Tests d’évaluation personnalisés</h3>
                     <p>Stockez et accédez aux bilans spécifiques à votre métier. Plus besoin d’imprimer vos grilles ou de noter vos observations sur papier.
@@ -29,12 +24,6 @@ const Service = () => {
                     <img src={`${prefix}/image/rendez-vous.png`} alt="" />
                     <h3>Gestion des rendez-vous</h3>
                     <p>Planifiez, modifiez et annulez vos rendez-vous en quelques clics en consultant votre calendrier.
-                    </p>
-                </article>
-                <article className="align-center">
-                    <img src={`${prefix}/image/facturation.png`} alt="" />
-                    <h3>Facturation des actes</h3>
-                    <p>Automatisez la création, la consultation et l'impression de vos factures. Suivez vos paiements et vos revenues en temps réel.
                     </p>
                 </article>
                 <article className="align-center">
